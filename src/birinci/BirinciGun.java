b67f57c21fb4cb815cd469c8a10242e895b238ed
Nilus007 <nilufer.cavus@boun.edu.tr>
2020-11-18 22:28:36 +0300

    feature

--- src/birinci/BirinciGun.java
+++ src/birinci/BirinciGun.java
@@ -3,5 +3,11 @@ package birinci;
 public class BirinciGun {
 public static void main(String[] args) {
 	System.out.println("11:05- BirinciGun");
+	System.out.println("Ýkinci Commit");
+	System.out.println("10:11 Ýkinci gun");
+	System.out.println("10:26 Ýkinci gun");
+	System.out.println("10:35 Ýkinci gun");
+	System.out.println("10:26 - feature branch");
+
 }
 }
