d94bc3a0418f2e1af4a7196d54ea3e65a3ac057d
Nilus007 <74240940+Nilus007@users.noreply.github.com>
2020-11-19 20:11:20 +0300

    Update BirinciGun.java

--- src/birinci/BirinciGun.java
+++ src/birinci/BirinciGun.java
@@ -9,6 +9,6 @@ public class BirinciGun {
 		System.out.println("10:35 ikinci gun");
 		System.out.println("11:05 local");
 		System.out.println("ben burda conflict beklemiyorum");
-
+		System.out.println("11:23 github.com");
 	}
 }
