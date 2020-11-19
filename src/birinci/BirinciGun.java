53ed21e906439ccd65bf75d9da3e9d991049ed75
Nilus007 <nilufer.cavus@boun.edu.tr>
2020-11-19 08:38:58 +0300

    ikinci commit

--- src/birinci/BirinciGun.java
+++ src/birinci/BirinciGun.java
@@ -2,6 +2,8 @@ package birinci;
 
 public class BirinciGun {
 public static void main(String[] args) {
-	System.out.println("11:05- BirinciGun");
+	System.out.println("11:57- BirinciGun");
+	System.out.println("ikinci Commit");
+	
 }
 }
