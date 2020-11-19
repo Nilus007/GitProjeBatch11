857d73db61ad0fad0b28d1de9eaf172bb2422d66
Nilus007 <nilufer.cavus@boun.edu.tr>
2020-11-19 18:37:13 +0300

    ikinci gun 2

--- src/birinci/BirinciGun.java
+++ src/birinci/BirinciGun.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("11:57- BirinciGun");
 	System.out.println("ikinci Commit");
 	System.out.println("10:12 ikinci Commit");
-	
+	System.out.println("10:26 ikinci gun");
 	
 }
 }
