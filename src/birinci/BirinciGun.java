4454207c62632d2a600b46373a5661b673bbfce4
Nilus007 <74240940+Nilus007@users.noreply.github.com>
2020-11-19 19:36:06 +0300

    Update BirinciGun.java

--- src/birinci/BirinciGun.java
+++ src/birinci/BirinciGun.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("10:12 ikinci Commit");
 	System.out.println("10:26 ikinci gun");
 	System.out.println("10:35 ikinci gun");
+	System.out.println("11:03 github.com");
 }
 }
